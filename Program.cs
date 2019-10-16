a551724432c45f9c07957a6c0bf69be41fe83f37
tarikmiri <tarik.miri@cbre.com>
2019-10-16 10:29:55 +0100

    relevant commit message

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace UselessApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! I am useless");
+            //Useless change
         }
     }
 }
