5133ce3df61843fba9c2ba09a45362aa14c35878
tarikmiri <tarik.miri@cbre.com>
2019-10-16 10:27:00 +0100

    Initial commit UselessApplication

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UselessApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! I am useless");
+        }
+    }
+}
